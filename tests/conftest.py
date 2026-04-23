308b5f80bd2546bbda97712395ba9a441bf89371
Juanjo Rivera <juanjoriverar@gmail.com>
2026-04-23 16:03:40 -0600

    Implemented register attempt with duplicated email

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,22 @@
 import pytest
-from collections.abc import Generator
+
+from datetime import date
 from sqlmodel import create_engine, Session, SQLModel
 from app.core.config import settings
 from fastapi.testclient import TestClient
 from app.main import app
 from app.db.session import get_db_session
+from app.schemas.user import UserRegister
 
 engine = create_engine(settings.dev_db_connection_str)
 
 @pytest.fixture
 def test_client():
+    SQLModel.metadata.drop_all(engine)
+    SQLModel.metadata.create_all(engine)
+    
     def override():
         with Session(engine) as session:
-            SQLModel.metadata.drop_all(engine)
-            SQLModel.metadata.create_all(engine)
             yield session
 
     app.dependency_overrides[get_db_session] = override
@@ -21,3 +24,19 @@ def test_client():
         yield c
     
     app.dependency_overrides.clear()
+
+@pytest.fixture
+def registered_user(test_client):
+    user = UserRegister(
+        email="test_user@testing.com",
+        password="TesTP@ssworD",
+        name="Test User",
+        date_of_birth=date(1991, 5, 9)
+    )
+
+    response = test_client.post("/auth/register", json=user.model_dump(mode="json"))
+
+    if response.status_code == 201:
+        access_token = response.json()["access_token"]
+        yield {"user": user, "access_token": access_token}
+    
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,15 +1,28 @@
 from datetime import date
 from app.schemas.user import UserRegister
 
-def test_user_register(test_client):
+def test_user_register_success(test_client):
     user = UserRegister(
         email="test_email2@email.com",
         password="TeSt_P@ssWorD",
         name= "Test User",
         date_of_birth=date(1991, 5, 12)
     )
+
     response = test_client.post("/auth/register", json=user.model_dump(mode="json"))
-    print("### ", response.json())
-    print("### ", user.model_dump_json())
     assert response.status_code == 201
     assert response.json()["message"] == "User created"
+
+def test_user_register_duplicated_email(test_client, registered_user):
+    db_user = registered_user["user"]
+    print(db_user.model_dump_json())
+    user = UserRegister(
+        email=db_user.email,
+        password="TeSt_P@ssWorD",
+        name= "Test User",
+        date_of_birth=date(1991, 5, 12)
+    )
+
+    response = test_client.post("/auth/register", json=user.model_dump(mode="json"))
+    assert response.status_code == 409
+    assert response.json()["detail"] == "An user with this email already exists"
