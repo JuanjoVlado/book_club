58cfeeca6d1db7c818f5a1cfeec72b0bd7520003
Juanjo Rivera <juanjoriverar@gmail.com>
2026-04-23 19:48:11 -0600

    Implemented auth_test.py. M1 Completed

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,5 @@
 from datetime import date
-from app.schemas.user import UserRegister
+from app.schemas.user import UserRegister, UserLogin
 
 def test_user_register_success(test_client):
     user = UserRegister(
@@ -15,7 +15,7 @@ def test_user_register_success(test_client):
 
 def test_user_register_duplicated_email(test_client, registered_user):
     db_user = registered_user["user"]
-    print(db_user.model_dump_json())
+    
     user = UserRegister(
         email=db_user.email,
         password="TeSt_P@ssWorD",
@@ -26,3 +26,60 @@ def test_user_register_duplicated_email(test_client, registered_user):
     response = test_client.post("/auth/register", json=user.model_dump(mode="json"))
     assert response.status_code == 409
     assert response.json()["detail"] == "An user with this email already exists"
+
+def test_user_login_success(test_client, registered_user):
+    db_user = registered_user["user"]
+    login_user = UserLogin(
+        email=db_user.email,
+        password=db_user.password
+    )
+    response = test_client.post(
+        "/auth/login",
+        data={"username": login_user.email, "password": login_user.password}
+    )
+
+    assert response.status_code == 200
+
+
+def test_user_login_invalid_email(test_client):
+    login_user = UserLogin(
+        email="NotExists@email.com",
+        password="anyPassword"
+    )
+    response = test_client.post(
+        "/auth/login",
+        data={"username": login_user.email, "password": login_user.password}
+    )
+
+    assert response.status_code == 401
+    assert response.json()["detail"] == "incorrect credentials"
+
+def test_user_login_invalid_password(test_client, registered_user):
+    db_user = registered_user["user"]
+    login_user = UserLogin(
+        email=db_user.email,
+        password=db_user.password+"x"
+    )
+    response = test_client.post(
+        "/auth/login",
+        data={"username": login_user.email, "password": login_user.password}
+    )
+
+    assert response.status_code == 401
+    assert response.json()["detail"] == "incorrect credentials"
+
+def test_user_logout_success(test_client, registered_user):
+    access_token = registered_user["access_token"]
+    response = test_client.post(
+        "/auth/logout",
+        headers={"Authorization": f"Bearer {access_token}"}
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {"message": "logout"}
+
+def test_user_logout_unsuccess(test_client):
+    response = test_client.post("/auth/logout")
+
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Not authenticated"}
